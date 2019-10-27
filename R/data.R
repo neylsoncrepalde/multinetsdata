7ed4d952628f796b9b75eada51696f51ec8c53d3
Neylson Crepalde <neylsoncrepalde@Neylsons-MacBook-Air.local>
2019-10-27 20:24:49 -0300

    first commit

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' Orchestral Musicians Multilevel Network
+#'
+#' A multilevel network of orchestras, musicians and culture organizations. Fisrt
+#'   published in Crepalde (2019). This is a real dataset. Data
+#'   was collected from september to december 2018. One should use the \code{multinets}
+#'   package functions to properly plot this data.
+#'
+#' @name orch_music_multilevel
+#'
+#' @format \code{orch_music_multilevel} is a graph object with 254 vertices and 629 edges.
+#'   There are three vertex attributes:
+#'
+#'   "type" defines to which level each vertex
+#'   belongs. \code{FALSE} means lower level and \code{TRUE} means higher level.
+#'
+#'   "name" is a random name generated to all vertices (chr).
+#'
+#'   "sex" is the sex of each individual in the lower level (chr).
+#'
+#'   There is one edge attribute, "relation", that states which type of relation two
+#'   individuals have. One of "Friendship", "Invitation" to play together in a concert,
+#'   "Recommendation" for a job or "Advice".
+#'
+#' @source Elaborated by the author.
+#'
+#' @seealso \code{\link[multinets]{is_multilevel}}, \code{\link[multinets]{set_color_multilevel}},
+#'   \code{\link[multinets]{set_shape_multilevel}}, \code{\link[multinets]{layout_multilevel}}
+#'
+#' @author Neylson Crepalde, \email{neylsoncrepalde@@gmail.com}
+#'
+#' @references CREPALDE, Neylson (2019). Social Construction of Quality in an Orchestral
+#'   Music Market. PhD Dissertation. Minas Gerais Federal University. Minas Gerais, Brazil.
+#'
+#'
+"orch_music_multilevel"
---
+++ cran_comments.md
@@ -0,0 +1,17 @@
+## First release
+
+This is a dataset package.
+
+## Test environments
+* local MacOS High Sierra 10.13.6, R 3.6.1
+* win-builder (devel and release)
+
+## R CMD check results
+
+0 errors | 0 warnings | 0 notes
+
+win-builder check: 0 errors | 0 warnings | 1 note
+
+**R Under development (unstable) (2017-09-12 r73242)**
+
+  * This is a new release.
